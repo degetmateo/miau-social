e316b7b649c6845be1baf92abf18f160531ca8b6
Mateo Deget <degetmateo@gmail.com>
2025-05-16 00:35:29 -0300

    date post

--- src/Server.ts
+++ src/Server.ts
@@ -109,7 +109,7 @@ export default class Server {
                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
                 "font-src 'self' https://fonts.gstatic.com http://localhost:4000; " +
                 "img-src 'self' https://www.gstatic.com https://i.ibb.co https://media.tenor.com https://animesher.com https://pbs.twimg.com https://social-miau.onrender.com http://localhost:4000 blob: data:; " +
-                "connect-src 'self' https://www.google.com data:; " +
+                "connect-src 'self' https://www.google.com blob: data:; " +
                 "frame-src https://www.google.com;"
               );
             next();
